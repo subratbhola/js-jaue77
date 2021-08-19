5ecd83908530fd8a96480fc752a2df32d6aaffaf
subratbhola <subratbhola07@gmail.com>
2021-08-19 20:38:46 +0530

    map, filter, reduce method

--- index.js
+++ index.js
@@ -4,4 +4,55 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-console.log("hello from closure function")
+console.log("hello from closure function");
+
+var arr = [10,20, 30, 50, 40];
+const arrayofobject = [ 
+
+  { 
+    "firstname": "subrat", 
+    "lastname": "Bhola", 
+    "age": 26 
+  }, 
+  { 
+    "firstname": "uday", 
+    "lastname": "emkolla", "age": 31
+  }, 
+  { 
+    "firstname": "melwin", 
+    "lastname": "Manaza", 
+    "age": 50 
+  }, 
+  { 
+    "firstname": "shurti", 
+    "lastname": "yadav", 
+    "age": 26 
+  }, 
+
+  ] 
+var output = arr.map((x)=>x*2);
+
+var resuceOutput = arrayofobject.reduce(function (accu, curr) {
+  if (curr.age > 30) {
+    accu.push(curr.firstname)
+  }
+  return accu;
+},[]) ;
+
+// using map, filter
+
+const firstnames = arrayofobject.filter((x)=>x.age >30).map((y)=>y.firstname);
+console.log(output);
+console.log(resuceOutput);
+console.log(firstnames);
+
+//maximum value of an array
+
+var maxValue = arr.reduce((max, curr)=> {
+  if (curr > max) {
+    max = curr;
+  }
+  return max;
+},0);
+
+console.log(maxValue);
