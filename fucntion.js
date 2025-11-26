f58b80fc251ef0b049db1f587603d897997c2910
subratbhola <git@stackblitz.com>
2025-11-26 17:12:14 +0530

    2 files modified

--- fucntion.js
+++ fucntion.js
@@ -1,12 +1,20 @@
 // function currying
 
 function abc(x) {
-  return function cbv(y){
-    console.log( x+y)
-  }
+  return function cbv(y) {
+    console.log(x + y);
+  };
 }
 
-let output =  abc(4);
+let output = abc(4);
 // let output = abc(4, 5); // this will not work
-console.log('output function currying'+ output);
+console.log('output function currying' + output);
 output(5);
+
+///
+function test() {
+  console.log(test.x);
+}
+test();
+test.x = 100;
+test();
